a3398f05b2a9d78c5a2be03e6a33dce210e6c220
DioptricDesign <contact@dioptricdesign.com>
2020-08-23 10:40:41 -0600

    Updated output menu path and dmenu colors.

--- dmen.sh
+++ dmen.sh
@@ -3,4 +3,4 @@
 # Import the colors
 . "${HOME}/.cache/wal/colors.sh"
 
-dmenu_run -nb "$color0" -nf "$color15" -sb "$color1" -sf "$color15" -h '20'
+dmenu_run -nb "$color1" -nf "$color15" -sb "$color2" -sf "$color15" -h '20'
--- outputmenu.sh
+++ outputmenu.sh
@@ -10,7 +10,7 @@ options="$option0\n$option1"
 selected="$(echo -e "$options" | rofi -lines 2 -dmenu -p "Audio Output")"
 case $selected in
     $option0)
-        ~/Documents/Scripts/sink.sh 1 && notify-send 'Input Changed' 'Stereo-Speakers Activated';;
+        ~/.local/share/scripts/sink.sh 1 && notify-send 'Input Changed' 'Stereo-Speakers Activated';;
     $option1)
-        ~/Documents/Scripts/sink.sh 0 && notify-send 'Input Changed' 'Head-Phones Activated';;
+        ~/.local/share/scripts/sink.sh 0 && notify-send 'Input Changed' 'Head-Phones Activated';;
 esac
