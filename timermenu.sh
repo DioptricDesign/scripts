04ff6e0a2f64ccadef9219a7e65dc6c25c37745d
DioptricDesign <contact@dioptricdesign.com>
2020-08-29 10:08:27 -0600

    timermenu update

--- timermenu.sh
+++ timermenu.sh
@@ -3,21 +3,21 @@
 # options to be displayed
 option0="30 seconds"
 option1="60 seconds"
-option2="30 Minutes"
-option3="1 Hour"
-option4="10 Minutes"
+option2="10 Minutes"
+option3="30 Minutes"
+option4="1 Hour"
 options="$option0\n$option1\n$option2\n$option3\n$option4"
 
-selected="$(echo -e "$options" | rofi -lines 4 -dmenu -p "Timers")"
+selected="$(echo -e "$options" | rofi -lines 5 -dmenu -p "Timers")"
 case $selected in
     $option0)
         sleep 30 && notify-send "time is  up" "30 seconds";;
     $option1)
         sleep 60 && notify-send "time is  up" "60 seconds";;
     $option2)
-        sleep 30m && notify-send "time is  up" "30 minutes";;
-    $option3)
-        sleep 1h && notify-send "time is  up" "1 hour";;
-    $option4)
         sleep 10m && notify-send "time is up" "10 minutes";;
+    $option3)
+        sleep 30m && notify-send "time is  up" "30 minutes";;
+    $option4)
+        sleep 1h && notify-send "time is  up" "1 hour";;
 esac
