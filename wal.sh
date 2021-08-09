5a70cb48d54305b0767a0fd27063ecf8adc2606a
DioptricDesign <58147329+DioptricDesign@users.noreply.github.com>
2021-08-09 14:59:35 -0600

    updated restart command

--- wal.sh
+++ wal.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 DISPLAY=:0 wal -i ~/.local/share/backgrounds/ -n ; feh --no-fehbg --bg-fill "$(< "${HOME}/.cache/wal/wal")"
 dunstcolor 
-qtile-cmd -o cmd -f restart
+qtile cmd-boj -o cmd -f restart
