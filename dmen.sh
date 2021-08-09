34cc19bad2da90b0a3f2a8b973525f3e1be77289
DioptricDesign <58147329+DioptricDesign@users.noreply.github.com>
2021-08-09 12:53:25 -0600

    background color changed

--- dmen.sh
+++ dmen.sh
@@ -3,4 +3,4 @@
 # Import the colors
 . "${HOME}/.cache/wal/colors.sh"
 
-dmenu_run -nb "$color1" -nf "$color15" -sb "$color2" -sf "$color15" -h '20'
+dmenu_run -nb "#282828" -nf "$color15" -sb "$color2" -sf "$color15" -h '20'
