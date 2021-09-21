e046d12bcfebfef45d7c66dbb6e195360b65a79d
DioptricDesign <58147329+DioptricDesign@users.noreply.github.com>
2021-09-21 16:33:36 -0600

    Update dmenusearch.sh

--- dmenusearch.sh
+++ dmenusearch.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 . "${HOME}/.cache/wal/colors.sh"
 search='https://duckduckgo.com/?q='
-query=$(echo '' | dmenu -p "Search:" -nb "#282828" -nf "$color15" -sb "$color2" -sf "$color15" -h '20' -fn "Noto Sans")
+query=$(echo '' | dmenu -p "Search:" -nb "#282828" -nf "$color15" -sb "$color2" -sf "$color15" -fn "Noto Sans")
 if [ -n "$query" ]; then
   xdg-open "${search}${query}" 2> /dev/null
 fi
