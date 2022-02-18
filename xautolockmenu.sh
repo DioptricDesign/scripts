008d13af54aa5845a9e5deaa890116202935c121
DioptricDesign <58147329+DioptricDesign@users.noreply.github.com>
2022-02-18 08:26:49 -0700

    Update xautolockmenu.sh

--- xautolockmenu.sh
+++ xautolockmenu.sh
@@ -1,16 +1,24 @@
 #!/bin/bash
 
 # options to be displayed
+
 option0="On"
+
 option1="Off"
 
 # options to be displyed
+
 options="$option0\n$option1"
 
-selected="$(echo -e "$options" | rofi -lines 2 -dmenu -p "Autolock")"
+selected="$(echo -e "$options" | rofi -lines 1 -columns 2 -width 20 -dmenu -p " Autolock")"
+
 case $selected in
+
     $option0)
+
       xautolock -time 5 -locker "lock" & notify-send 'Screen Lock Enabled' 'Locking When idle 5 minutes';;
+
     $option1)
+
       xautolock -exit & notify-send 'Screen Lock Disabled' 'Remember to lock it yourself.';;
 esac
