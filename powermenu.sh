8604f36d83b52a629e11d5e3791c03e8a27f1806
DioptricDesign <58147329+DioptricDesign@users.noreply.github.com>
2022-02-18 08:32:58 -0700

    Update powermenu.sh

--- powermenu.sh
+++ powermenu.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 # options to be displayed
-option0="Lock"
-option1="Logout"
-option2="Suspend"
-option3="Reboot"
-option4="Shutdown"
+option0=" Lock"
+option1=" Logout"
+option2=" Suspend"
+option3=" Reboot"
+option4=" Shutdown"
 
 # options passed into variable
 options="$option0\n$option1\n$option2\n$option3\n$option4"
 
-chosen="$(echo -e "$options" | rofi -lines 5 -dmenu -p "Power")"
+chosen="$(echo -e "$options" | rofi -lines 5 -dmenu -width -30 -p "Power")"
 case $chosen in
     $option0)
         lock;;
@@ -20,6 +20,6 @@ case $chosen in
         systemctl suspend;;
     $option3)
         systemctl reboot;;
-	  $option4)
+    $option4)
         killall qtile & systemctl poweroff;;
 esac
