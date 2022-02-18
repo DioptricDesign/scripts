0b9f83e0c4c7900147f9c475e873941b45d384d4
DioptricDesign <58147329+DioptricDesign@users.noreply.github.com>
2022-02-18 08:31:33 -0700

    Update scrotmenu.sh

--- scrotmenu.sh
+++ scrotmenu.sh
@@ -8,7 +8,7 @@ option2="Window"
 # options to be displyed
 options="$option0\n$option1\n$option2"
 
-selected="$(echo -e "$options" | rofi -lines 3 -dmenu -p "Screen Shot")"
+selected="$(echo -e "$options" | rofi -lines 3 -width 30 -dmenu -p " Screen Shot")"
 case $selected in
     $option0)
         cd ~/Pictures/scrots/ && sleep 1 && scrot;;
