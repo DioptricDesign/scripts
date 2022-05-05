bbf32c5edbfec37f88906cc323df808ae77adaea
DioptricDesign <58147329+DioptricDesign@users.noreply.github.com>
2022-05-05 07:41:03 -0600

    Update outputmenu.sh

--- outputmenu.sh
+++ outputmenu.sh
@@ -7,7 +7,7 @@ option1="Head-Phones"
 # options to be displyed
 options="$option0\n$option1"
 
-selected="$(echo -e "$options" | rofi -lines 1 -columns 2 -width 20 -dmenu -p "Audio Output")"
+selected="$(echo -e "$options" | rofi -dmenu -p "Audio Output")"
 case $selected in
     $option0)
         ~/.local/share/scripts/sink.sh 1 && notify-send 'Output Changed' 'Stereo-Speakers Activated';;
