92756da1b01107861302a747c9d07f86ffa4ecfd
DioptricDesign <58147329+DioptricDesign@users.noreply.github.com>
2022-06-02 08:17:27 -0600

    Update hotkeys.sh

--- hotkeys.sh
+++ hotkeys.sh
@@ -1,2 +1,2 @@
  #!/bin/bash 
-zenity --list --title="Hotkeys" --column="Hotkeys" "$(sed -n 53,141p  ~/.config/qtile/config.py)" --no-markup --height=800 --width=600
+zenity --list --title="Hotkeys" --column="Hotkeys" "$(sed -n 60,144p ~/.config/qtile/config.py)" --no-markup --height=800 --width=600
